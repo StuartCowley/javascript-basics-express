96308e823776931512ec24ee5353dded0f950011
TonyMM55 <tonymmoran@gmail.com>
2023-02-11 13:19:53 +0000

    changed all the code in app.js to include imported functions from strings.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { sayHello } = require('./lib/strings');
 const { firstCharacters } = require('./lib/strings');
+const { uppercase } = require('./lib/strings');
+const { lowercase } = require('./lib/strings');
+const { firstCharacter } = require('./lib/strings');
+
 const app = express();
 
 app.get('/strings/hello/world', (_, res) => {
@@ -12,18 +16,18 @@ app.get('/strings/hello/:string', (req, res) => {
 });
 
 app.get('/strings/upper/:string', (req, res) => {
-  const result = req.params.string.toUpperCase();
-  res.status(200).json({ result });
+  // const result = req.params.string.toUpperCase();
+  res.status(200).json({ result: uppercase(req.params.string) });
 });
 
 app.get('/strings/lower/:string', (req, res) => {
-  const result = req.params.string.toLowerCase();
-  res.status(200).json({ result });
+  // const result = req.params.string.toLowerCase();
+  res.status(200).json({ result: lowercase(req.params.string)});
 });
 
 app.get('/strings/first-character/:string', (req, res) => {
-  const result = req.params.string.charAt(0);
-  res.status(200).json({ result });
+  // const result = req.params.string.charAt(0);
+  res.status(200).json({ result: firstCharacter(req.params.string)});
 });
 
 app.get('/strings/first-characters/:string', (req, res) => {
