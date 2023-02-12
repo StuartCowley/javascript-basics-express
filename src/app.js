5505718e03d92ca1c7677b64cdb71f52aa50fe5d
Matthew Giles <matty92g@hotmail.co.uk>
2023-02-12 16:34:56 +0000

    Hangling Request Bodies

--- src/app.js
+++ src/app.js
@@ -1,14 +1,46 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings');
+const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings');
+const { add } = require('./lib/numbers');
 
 const app = express();
+const router = express.Router();
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.get('/strings/hello/:id', (req, res) => {
-  res.status(200).json({ result: sayHello(req.params.id) });
+// Strings
+
+app.get('/strings/hello/:str', (req, res) => {
+  const string = req.params.str;
+  res.status(200).json({ result: sayHello(string) });
+});
+
+app.get('/strings/upper/:str', (req, res) => {
+  const string = req.params.str;
+  res.status(200).json({ result: uppercase(string) });
+});
+
+app.get('/strings/lower/:str', (req, res) => {
+  const string = req.params.str;
+  res.status(200).json({ result: lowercase(string) });
+});
+
+app.get('/strings/first-characters/:str', (req, res) => {
+  const string = req.params.str;
+  const i = req.query.length;
+  const n = i > 0 ? i : 1;
+  res.status(200).json({ result: firstCharacters(string, n) });
+});
+
+// Numbers
+
+app.get('/numbers/add/:num1/and/:num2', (req, res) => {
+  const number1 = parseInt(req.params.num1);
+  const number2 = parseInt(req.params.num2);
+  return Number.isNaN(number1) || Number.isNaN(number2)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).json({ result: add(number1, number2) });
 });
 
 module.exports = app;
