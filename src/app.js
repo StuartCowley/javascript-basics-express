86592dad8e945dfa40ace9569d35a766e82bcd1a
Laura Crawford <lhcrawford97@gmail.com>
2023-02-14 16:02:12 +0000

    app res and req updates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+const strings = require('./lib/strings');
+
 const app = express();
+
+const helloWorld = {
+  result: strings.sayHello,
+};
+
+app.get('/strings/hello/:thing', (req, res) => {
+  const { thing } = req.params;
+  res.status(200);
+  res.send({ result: strings.sayHello(thing) });
+});
 
 module.exports = app;
