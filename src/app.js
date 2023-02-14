a9ac4e3e90c0bf0df572b782d2672516c70558f4
Laura Crawford <lhcrawford97@gmail.com>
2023-02-14 17:36:00 +0000

    made route handlers for strings test

--- src/app.js
+++ src/app.js
@@ -1,17 +1,40 @@
 const express = require('express');
 
+const qs = require('qs');
 const strings = require('./lib/strings');
 
 const app = express();
 
-const helloWorld = {
-  result: strings.sayHello,
-};
-
 app.get('/strings/hello/:thing', (req, res) => {
   const { thing } = req.params;
   res.status(200);
+
   res.send({ result: strings.sayHello(thing) });
+});
+
+app.get('/strings/upper/hello', (req, res) => {
+  res.status(200);
+  res.send({ result: strings.uppercase('hello') });
+});
+
+app.get('/strings/lower/HELLO', (req, res) => {
+  res.status(200);
+  res.send({ result: strings.lowercase('hello') });
+});
+
+// app.setting('query parser', function(str) {
+// return qs.parse(str, {
+/* custom options */
+//   });
+// });
+app.get('/strings/first-characters/:string', (req, res) => {
+  const { string } = req.params;
+  const { length } = req.query;
+  res.status(200);
+  if (length) {
+    res.send({ result: strings.firstCharacters(string, length) });
+  }
+  res.send({ result: strings.firstCharacter(string) });
 });
 
 module.exports = app;
