d6a3fe494f65c3b44e93dfbf6b45cd6a81e6d372
Laura Crawford <lhcrawford97@gmail.com>
2023-02-15 20:55:58 +0000

    route handlers for numbers testing

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 
-const qs = require('qs');
 const strings = require('./lib/strings');
+const numbers = require('./lib/numbers');
 
 const app = express();
+
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/strings/hello/:thing', (req, res) => {
   const { thing } = req.params;
   res.status(200);
-
   res.send({ result: strings.sayHello(thing) });
 });
 
@@ -22,11 +25,6 @@ app.get('/strings/lower/HELLO', (req, res) => {
   res.send({ result: strings.lowercase('hello') });
 });
 
-// app.setting('query parser', function(str) {
-// return qs.parse(str, {
-/* custom options */
-//   });
-// });
 app.get('/strings/first-characters/:string', (req, res) => {
   const { string } = req.params;
   const { length } = req.query;
@@ -37,4 +35,39 @@ app.get('/strings/first-characters/:string', (req, res) => {
   res.send({ result: strings.firstCharacter(string) });
 });
 
+app.get('/numbers/add/:a/and/:b', (req, res) => {
+  const { a } = req.params;
+  const { b } = req.params;
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400);
+    res.send({ error: 'Parameters must be valid numbers.' });
+  }
+  res.status(200);
+  res.send({ result: numbers.add(+a, +b) });
+});
+
+app.get('/numbers/subtract/:a/from/:b', (req, res) => {
+  const { a } = req.params;
+  const { b } = req.params;
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400);
+    res.send({ error: 'Parameters must be valid numbers.' });
+  }
+  res.status(200);
+  res.send({ result: numbers.subtract(Number(b), Number(a)) });
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  const { a, b } = req.body;
+  if (!a || !b) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" are required.' });
+  }
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400);
+    res.send({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+  res.status(200);
+  res.send({ result: numbers.multiply(Number(b), Number(a)) });
+});
 module.exports = app;
