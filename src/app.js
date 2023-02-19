62aa7d3c4e99a71d8734f9572b59c0a0102d0301
josephredmond5 <josephredmond5@icloud.com>
2023-02-19 20:37:49 +0000

    finished the strings part of javascript basics express

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { sayHello } = require('./lib/strings');
 
 const app = express();
+app.use(express.json());
+
+// STRINGS
 
 app.get('/strings/hello/world', (req, res) => {
   res.status(200);
@@ -25,5 +29,9 @@ app.get('/strings/first-characters/hello', (req, res) => {
 app.get('/strings/first-characters/:str', (req, res) =>
   res.json({ result: req.params.str.slice(0, req.query.length || undefined) }),
 );
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.json({ result: sayHello(req.params.string) });
+});
 
 module.exports = app;
