9983ac5c11acaa0b8c697113c95862090fd9d4c7
josephredmond5 <josephredmond5@icloud.com>
2023-02-20 15:51:22 +0000

    completed the javascript-basics-express module

--- src/app.js
+++ src/app.js
@@ -1,37 +1,114 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings');
 
 const app = express();
+
 app.use(express.json());
 
-// STRINGS
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
 
-app.get('/strings/hello/world', (req, res) => {
-  res.status(200);
-  res.json({ result: 'Hello, world!' });
-});
-
-app.get('/strings/upper/Hello', (req, res) => {
-  res.status(200);
-  res.json({ result: 'HELLO' });
-});
-
-app.get('/strings/lower/hello', (req, res) => {
-  res.status(200);
-  res.json({ result: 'hello' });
-});
-
-app.get('/strings/first-characters/hello', (req, res) => {
-  res.status(200);
-  res.json({ result: 'h' });
-});
-
-app.get('/strings/first-characters/:str', (req, res) =>
-  res.json({ result: req.params.str.slice(0, req.query.length || undefined) }),
-);
+const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 
 app.get('/strings/hello/:string', (req, res) => {
+  res.status(200);
   res.json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200);
+  res.json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200);
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    res.status(200).json({ result: firstCharacter(req.params.string) });
+  } else {
+    res.status(200).json({ result: firstCharacters(req.params.string, req.query.length) });
+  }
+});
+
+app.get('/numbers/add/:num1/and/:num2', (req, res) => {
+  const num1 = parseInt(req.params.num1);
+  const num2 = parseInt(req.params.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: add(num1, num2) });
+  }
+});
+app.get('/numbers/subtract/:num1/from/:num2', (req, res) => {
+  const num1 = parseInt(req.params.num2);
+  const num2 = parseInt(req.params.num1);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: subtract(num1, num2) });
+  }
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: multiply(a, b) });
+  }
+});
+
+app.post('/numbers/divide', (req, res) => {
+  if (req.body.a === 0) {
+    res.status(200).json({ result: 0 });
+  } else if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: divide(a, b) });
+  }
+});
+
+app.post('/numbers/remainder', (req, res) => {
+  if (req.body.a === 0) {
+    res.status(200).json({ result: 0 });
+  } else if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: remainder(a, b) });
+  }
 });
 
 module.exports = app;
