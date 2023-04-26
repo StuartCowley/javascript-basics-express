886ca53f00fc0e3836e33a73cc800ac7a5f55f37
Benj <ben.speight@outlook.com>
2023-04-26 19:13:03 +0100

    cloned repo

--- src/app.js
+++ src/app.js
@@ -2,4 +2,31 @@ const express = require('express');
 
 const app = express();
 
+const { sayHello } = require('./strings');
+const { uppercase } = require('./strings');
+const { lowercase } = require('./strings');
+const { firstCharacter } = require('./strings');
+const { firstCharacters } = require('./strings');
+
+app.get('/strings/hello/:string', (req, res) => {
+  res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.status(200).json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const n = req.query.length;
+  res.status(200).json({ result: firstCharacters(req.params.length, n) });
+});
+
 module.exports = app;
