d308baca4fb912046be91e9d57d82aaa82be9127
Jose Wills <jose.wills3@gmail.com>
2023-04-28 22:03:04 +0100

    completed strings and cutomising output

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const { sayHello } = require('./lib/strings');
 
 const app = express();
+
+app.get('/strings/hello/:id', (req, res) => {
+  res.status(200).json({
+    result: sayHello(req.params.id),
+  });
+});
 
 module.exports = app;
